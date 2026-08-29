47582c4b3cb805b7d30999cd75bd9a9205c25827
Leonardo Paiva <67243028+leonardo201800478@users.noreply.github.com>
2026-08-29 10:36:05 -0300

    test(v2): add clean bootstrap test

---
+++ v2/tests/test_bootstrap.py
@@ -0,0 +1,6 @@
+from serm_v2.gui.home import HomePage
+
+
+def test_home_class_exists():
+    """Keep the first V2 test focused on the clean Home boundary."""
+    assert HomePage.__name__ == "HomePage"
