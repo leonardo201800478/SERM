31788387eab226ace1bd059ffa0d6583e3c916b0
Leonardo Paiva <67243028+leonardo201800478@users.noreply.github.com>
2026-08-29 14:45:28 -0300

    refactor: expose Public DAT Catalog provider

--- v2/serm_v2/sources/acquisition/__init__.py
+++ v2/serm_v2/sources/acquisition/__init__.py
@@ -1,1 +1,10 @@
 """Acquisition backends used by SERM source integrations."""
+
+from .dat_catalog import DatCatalogEntry, DatCatalogError, DatStatus, PublicDatCatalogProvider
+
+__all__ = [
+    "DatCatalogEntry",
+    "DatCatalogError",
+    "DatStatus",
+    "PublicDatCatalogProvider",
+]
