7c7c8e31d5b569f44aef68fb41d94dcc3f16dd2c
Leonardo Paiva <67243028+leonardo201800478@users.noreply.github.com>
2026-08-29 14:43:42 -0300

    refactor: remove obsolete No-Intro source package

--- v2/serm_v2/sources/no_intro/errors.py
+++
@@ -1,9 +0,0 @@
-"""Errors raised by the No-Intro source adapter."""
-
-
-class NoIntroParseError(ValueError):
-    """Raised when a No-Intro DAT cannot be parsed as expected."""
-
-
-class NoIntroDownloadError(RuntimeError):
-    """Raised when a No-Intro DAT cannot be downloaded."""
