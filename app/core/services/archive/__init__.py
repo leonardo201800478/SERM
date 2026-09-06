d569c57ba6de51bb25930c6ed7e7913fa881c265
Leonardo Paiva <67243028+leonardo201800478@users.noreply.github.com>
2026-09-06 15:46:07 -0300

    chore(v2): remover codigo legado da V1

--- app/core/services/archive/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Infraestrutura unificada para arquivos compactados."""
-
-from .archive_detector import ArchiveDetector, ArchiveTool
-from .archive_service import ArchiveError, ArchiveService
-
-__all__ = ["ArchiveDetector", "ArchiveError", "ArchiveService", "ArchiveTool"]
--- app/gui/tabs/__init__.py
+++
@@ -1,4 +0,0 @@
-from .directories_tab import DirectoriesTab
-from .filters_tab_realtime import FiltersTab  # <-- agora importa a classe correta
-from .home_tab import HomeTab
-from .scan_roms_tab import ScanRomsTab
