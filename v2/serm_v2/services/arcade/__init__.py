daec159cfda3725e491b8757e61940ca25ad0ddf
Leonardo Paiva <67243028+leonardo201800478@users.noreply.github.com>
2026-09-07 12:52:23 -0300

    feat(v2): expose arcade set builder service

---
+++ v2/serm_v2/services/arcade/__init__.py
@@ -0,0 +1,17 @@
+"""Servicos do Arcade Studio."""
+
+from .set_builder import (
+    ArcadeSetBuilder,
+    SetBuildDecision,
+    SetBuildError,
+    SetBuildResult,
+    SetBuildTrace,
+)
+
+__all__ = [
+    "ArcadeSetBuilder",
+    "SetBuildDecision",
+    "SetBuildError",
+    "SetBuildResult",
+    "SetBuildTrace",
+]
